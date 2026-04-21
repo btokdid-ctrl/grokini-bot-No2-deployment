6866b9a27a9de5b151f4a0dd616567803b05703f
btokdid-ctrl <btokdid@gmail.com>
2026-04-21 06:30:12 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1409,7 +1409,7 @@ async function showMainMenu(ctx, edit = false) {
     }
     
     const message = `
-🚀 *Welcome to @WTFSnipeXBot Trading Bot* 🤖
+🚀 *Welcome to Trade Wiz Trading Bot* 🤖
 *I'm your Web3 execution engine*.
 AI-driven. Battle-tested. Locked down.
 ━━━━━━━━━━━━━━━━━━
